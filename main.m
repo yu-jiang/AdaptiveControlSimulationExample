b23ded8aabdbf5896bba30ebfc35dc29ae00d3ae
Yu Jiang <yjiang26@gmail.com>
2016-01-15 19:24:32 -0500

    refined the comments

--- main.m
+++ main.m
@@ -1,15 +1,21 @@
 function main()
-%% This is an example of the simulation of a simple adaptive 
+%% Main Function
+% This is an example of the simulation of a simple adaptive 
 % control system
+
+% Set parameter
 x0 = 1;         % Initial conidtion for the state
 theta0 = [0;0]; % Initial condition for the estimate
 a = 2; b = 3;   % Parameters unknown to the controller
 K = 1;          % controller parameter
 Gamma = diag([1,2]);    % controller parameter
+
+% Run simulation
 [t,y] = ode45(@(t,x)mySystem(t,x,a,b,K,Gamma), ...
     [0 100], ...  % Simulation time span
     [x0;theta0]);
 
+% Create figures
 figure
 subplot(311)
 plot(t,y(:,1),t,sin(t));
@@ -24,20 +30,27 @@ plot(t,y(:,3),  [0 t(end)], [b b])
 legend({'$\hat{b}$','$b$'},'Interpreter','latex');
 end
 
-% The function describing the syetem dynamics
+%% The function describing the syetem dynamics
 function dstate = mySystem(t,state,a,b,K,Gamma)
+% Extract the states
 x = state(1);
 htheta = state(2:3);
+
+% Create the reference tracking signal
 xd = sin(t);
 xdd = cos(t);
+
+% Compute error
 e = x - xd;
 
+% Formulate the controller
 u = -Local_psi(x)*htheta + xdd - K*e;
-
 dhtheta = Gamma*Local_psi(x)'*e;
 
+% Main dynamic system
 dx = -a*x*x + b*exp(-x) + u;
 
+% Combine the derivatives
 dstate = [dx; dhtheta];
 end
 
